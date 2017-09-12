809a553adb2916b791d114adc9c7ffefb50e58f5
Swathi Bhuvaneshwar Babu <s.bhuvaneshwarbabu@gmail.com>
2017-09-12 14:21:42 -0500

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,12 @@
+require "susy"
+
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+fonts_dir = "fonts"
+javascripts_dir =  "js"
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
